a2241bfa3a187127146bfdbb5ff3661fa57fc5ed
Anubhav Sarkar <v-ansark@microsoft.com>
2021-01-18 09:01:33 +0530

    Removed spinning rectangle

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Categories from "./components/Categories/Categories";
 const App: React.FC<{}> = () => {
   return (
     <div className="App">
-      <div className="top-rectangle"></div>
       <Categories />
     </div>
   );
